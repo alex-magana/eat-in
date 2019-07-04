a9af7fbe7aa3b0765716fd182141ddecd99189ad
alex-magana <alex.magana@andela.com>
2019-07-04 16:48:59 +0300

    Add restaurant validation matchers

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ApplicationRecord
+  # model association
+  has_many :items, dependent: :destroy
+
+  # validations
+  validates_presence_of :name, :opening_time, :closing_time, :created_by
 end

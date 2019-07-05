f24b026fe73c5c3b3663f33e671a6d95821a16a3
alex-magana <alex.magana@andela.com>
2019-07-05 17:00:17 +0300

    Rectify naming

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -7,7 +7,7 @@ module ExceptionHandler
       json_response({ message: e.message }, :not_found)
     end
 
-    rescue_from AcriveRecord::RecordInvalid do |e|
+    rescue_from ActiveRecord::RecordInvalid do |e|
       json_response({ message: e.message }, :unprocessable_entity )
     end
   end

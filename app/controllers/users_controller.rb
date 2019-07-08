519068bd494aa2cd40bec40566249522d9387114
alex-magana <alex.magana@andela.com>
2019-07-09 00:56:04 +0300

    Define Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  # POST /signup
+  # return authenticated token upon signup
+  def create
+    # create! raises ActiveRecord::RecordInvalid unlike create
+    user = User.create!(user_params)
+    auth_token = Authenticate.new(user.email, user.password).call
+    response = { message: Message.account_created, auth_token: auth_token }
+    json_response(response)
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
 end

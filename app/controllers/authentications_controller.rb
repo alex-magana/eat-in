2bbaefcaf45309169b4859863cfe51447173357d
alex-magana <alex.magana@andela.com>
2019-07-08 23:18:56 +0300

    Define AuthenticationsController

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,2 +1,18 @@
+# This controller exposes a route /auth/login
+# that accepts suer credentials and returns a JSON response with
+# the result
+
 class AuthenticationsController < ApplicationController
+  # return an auth token once user is authenticated
+  def authenticate
+    auth_token =
+      AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    json_response(auth_token: auth_token)
+  end
+
+  private
+
+  def auth_params
+    paams.permit(:emai, :password)
+  end
 end

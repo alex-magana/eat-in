136b39d3970acfc5cf89e19b13dcb3af18759f03
alex-magana <alex.magana@andela.com>
2019-07-13 23:27:39 +0300

    Add serializer

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,3 +1,8 @@
 class RestaurantSerializer < ActiveModel::Serializer
-  attributes :id
+  # attributes to be serialized
+  attributes :id, :name, :opening_time, :closing_time,
+             :created_by, :created_at, :updated_at
+
+  # model association
+  has_many :items
 end

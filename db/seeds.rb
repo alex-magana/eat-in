b50d6a82f7c684b77716be9f8425c7c24017ecec
alex-magana <alex.magana@andela.com>
2019-07-13 23:56:32 +0300

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# seeds for 50 records
+50.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.unique.name,
+    opening_time: Faker::Number.within(8..12),
+    closing_time: Faker::Number.within(13..23),
+    created_by: User.first.id
+  )
+  restaurant.items.create(
+    name: Faker::Food.dish,
+    price: Faker::Number.decimal(4, 3),
+    available: Faker::Boolean.boolean
+  )
+end
